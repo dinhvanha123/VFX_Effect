353ad1743bdac0c562b28fc1296a47500885f961
BillyTheKid <dinhvanha1995@gmail.com>
2020-10-24 10:46:20 +0700

    Test Pull Request

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class TestScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    Transform trans;
     void Start()
     {
-        
+        trans = GetComponent<Transform>();
     }
 
     // Update is called once per frame
